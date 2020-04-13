65627e3371ba1922f1d82c8491b318a79de388df
Matt Gordon <bigmattsize21@gmail.com>
2020-04-13 21:57:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ class Scraper
     
     html = Nokogiri::HTML(open(index_url))
     
-    html.css(".student-card").each do |student|
+    html.css(".student-card a").each do |student|
       hash = {:name => student.css("h4.student-name").text, :location => student.css("p.student-location").text, :profile_url => student.css("a").attribute("href").value
       }
       students_hash << hash
